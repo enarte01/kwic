acb086428067fb5c2855e0838cccbe11038ecaaf
Edmund Nartey <enarte01@mail.bbk.ac.uk>
2016-03-30 20:41:15 +0100

    first

---
+++ Documents/Java/KWIIC/src/kwic/Kwic.scala
@@ -0,0 +1,17 @@
+package kwic
+import java.io._
+import scala.io.Source
+import scala.swing.FileChooser
+import scala.collection.mutable.Map
+
+
+
+object Kwic {
+
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+}
